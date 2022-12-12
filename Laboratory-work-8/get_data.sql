9a66ca426c18a3e32ac8bfe6cad6daf223eb9426
Sergeev1ch <58373600+Sergeev1ch@users.noreply.github.com>
2022-12-12 14:07:44 +0200

    Update get_data.sql

--- Laboratory-work-8/get_data.sql
+++ Laboratory-work-8/get_data.sql
@@ -1,11 +1,11 @@
-CREATE OR REPLACE FUNCTION get_data (model_name VARCHAR)
+CREATE OR REPLACE FUNCTION get_data (post_name VARCHAR)
 RETURNS TABLE
 (t_id INTEGER, name VARCHAR, post VARCHAR)
 AS $$
 DECLARE
 	str VARCHAR;
 BEGIN
-	str := 'SELECT * from teacher where post = "||post_name||" ';
+	str := 'SELECT t_id, name, post FROM teacher WHERE post = ''' || post_name || '''';
 	RAISE NOTICE 'Query=%' ,str;
 	RETURN QUERY EXECUTE str;
 END;
