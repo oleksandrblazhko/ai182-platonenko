627dd441f2d70682cda9874e6085c8c1398b1419
Sergeev1ch <58373600+Sergeev1ch@users.noreply.github.com>
2022-12-12 15:03:46 +0200

    Update get_data_secure.sql

--- Laboratory-work-8/get_data_secure.sql
+++ Laboratory-work-8/get_data_secure.sql
@@ -3,7 +3,7 @@ RETURNS TABLE (t_id INTEGER, name VARCHAR, post VARCHAR) AS $$
 DECLARE
 	str VARCHAR;
 BEGIN
-	str := 'SELECT t_id, name, post FROM teacher WHERE name = $1';
+	str := 'SELECT t_id, name, post FROM teacher WHERE post = $1';
 	RAISE NOTICE 'Query=%', str;
 	RETURN QUERY EXECUTE str USING post_name;
 END;
